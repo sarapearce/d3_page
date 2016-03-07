97dc72329f77deeb47fa329e46e4d6d28ad3439a
Sara Pearce <sarapearce3.14@gmail.com>
2016-03-07 10:08:31 -0600

    add config file

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 //    addEvents();
 //    popGraph();
-//    barGraph();
-      rects();
+    barGraph();
+//      rects();
 });
 
 function addEvents() {
--- php/index.php
+++ php/index.php
@@ -2,38 +2,28 @@
 <?php
 
 class Template {
+    
+    public function connectDb() {
+	$config = include('config.php');
+    }
+
     public function loadSmartyAction() {
 	require_once('libs/Smarty.class.php');
-	$smarty = new Smarty();
+	$this->smarty = new Smarty();
     }
-
+ 
     public function compileTemplates() {
 	// force compilation of all template files
-	$smarty->compileAllTemplates('.tpl', true);
+	$this->smarty->compileAllTemplates('.tpl', true);
     }
 
-    public function loadTemplate() {
-	$smarty->caching = true;
-
-	if (!$smarty->is_cached('index.tpl')) {
-
-	    // dummy up some data
-	    $address = '245 N 50th';
-	    $db_data = array(
-		'City' => 'Lincoln',
-		'State' => 'Nebraska',
-		'Zip' => '68502'
-	    );
-
-	    $smarty->assign('Name', 'Fred');
-	    $smarty->assign('Address', $address);
-	    $smarty->assign('data', $db_data);
-	}
-
+    public function displayIndexTemplate() {
 	// display the output
-	$smarty->display('index.tpl');
+	$this->smarty->display('index.tpl');
     }
 
 }
+
+
 ?>
 
